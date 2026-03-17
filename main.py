ae33c07f7213827c2af5c98c457c098675fd56d3
Dit Navn <din@email.dk>
2026-03-17 03:50:31 +0100

    subplot

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from collections import deque
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-PORT = "/dev/cu.wchusbserial56440196321"   # skift til din port
+PORT = "COM4"   # skift til din port
 BAUD = 115200
-CSV_FILE = "imu_walk1.csv"
+CSV_FILE = "imu_walk_rotatel42.csv"
 MAX_POINTS = 1000
 
 ser = serial.Serial(PORT, BAUD, timeout=1)
@@ -27,19 +27,25 @@ gyro_z_vals = deque(maxlen=MAX_POINTS)
 
 counter = 0
 
-fig, ax = plt.subplots()
-line1, = ax.plot([], [], label="acc_x")
-line2, = ax.plot([], [], label="acc_y")
-line3, = ax.plot([], [], label="acc_z")
-line4, = ax.plot([], [], label="gyro_x")
-line5, = ax.plot([], [], label="gyro_y")
-line6, = ax.plot([], [], label="gyro_z")
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
+line1, = ax1.plot([], [], label="acc_x")
+line2, = ax1.plot([], [], label="acc_y")
+line3, = ax1.plot([], [], label="acc_z")
 
-ax.set_title("ESP32 IMU live data")
-ax.set_xlabel("Sample")
-ax.set_ylabel("Value")
-ax.legend()
-ax.grid(True)
+line4, = ax2.plot([], [], label="gyro_x")
+line5, = ax2.plot([], [], label="gyro_y")
+line6, = ax2.plot([], [], label="gyro_z")
+
+ax1.set_title("ESP32 IMU live data")
+ax1.set_xlabel("Sample")
+ax1.set_ylabel("Acceleration")
+ax1.legend()
+ax1.grid(True)
+
+ax2.set_xlabel("Sample")
+ax2.set_ylabel("Gyroscope")
+ax2.legend()
+ax2.grid(True)
 
 def update(frame):
     global counter
@@ -79,19 +85,29 @@ def update(frame):
         line1.set_data(x_vals, acc_x_vals)
         line2.set_data(x_vals, acc_y_vals)
         line3.set_data(x_vals, acc_z_vals)
+        
         line4.set_data(x_vals, gyro_x_vals)
         line5.set_data(x_vals, gyro_y_vals)
         line6.set_data(x_vals, gyro_z_vals)
 
-        ax.set_xlim(min(x_vals), max(x_vals) if max(x_vals) > 0 else 1)
+        x_min = min(x_vals)
+        x_max = max(x_vals) if max(x_vals) > 0 else 1
+        ax1.set_xlim(x_min, x_max)
+        ax2.set_xlim(x_min, x_max)
+        
 
-        all_vals = (
-            list(acc_x_vals) + list(acc_y_vals) + list(acc_z_vals) +
-            list(gyro_x_vals) + list(gyro_y_vals) + list(gyro_z_vals)
-        )
-        y_min = min(all_vals) - 0.5
-        y_max = max(all_vals) + 0.5
-        ax.set_ylim(y_min, y_max)
+        acc_all = list(acc_x_vals) + list(acc_y_vals) + list(acc_z_vals)
+        acc_min = min(acc_all) - 0.5
+        acc_max = max(acc_all) + 0.5
+        ax1.set_ylim(acc_min, acc_max)
+
+
+        gyro_all = list(gyro_x_vals) + list(gyro_y_vals) + list(gyro_z_vals)
+        gyro_min = min(gyro_all) - 0.5
+        gyro_max = max(gyro_all) + 0.5
+        ax2.set_ylim(gyro_min, gyro_max)
+
+
 
     return line1, line2, line3, line4, line5, line6
 
